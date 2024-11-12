1096208d5c33aaf2ed8c3a4f9bc6a455eb56e4e8
Arshia <arshia@Arshias-MacBook-Pro.local>
2024-11-12 22:58:05 +0330

    feat: adding Builder annotation to Transaction

--- Fesadyab/src/main/java/domain/Transaction.java
+++ Fesadyab/src/main/java/domain/Transaction.java
@@ -1,10 +1,12 @@
 package domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Transaction {
     int transactionId;
     int accountId;
