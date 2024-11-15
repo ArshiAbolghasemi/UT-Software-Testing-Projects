32569fd16a4a7770b4dd42114e6bb9ca506c439f
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2024-11-15 13:02:43 +0330

    change transactionId type from int to long

--- Fesadyab/src/test/java/util/TransactionFaker.java
+++ Fesadyab/src/test/java/util/TransactionFaker.java
@@ -1,14 +1,21 @@
 package util;
 
+import java.time.Instant;
+
 import com.github.javafaker.Faker;
 
 import domain.Transaction;
 
 public class TransactionFaker {
 
+    private static long generateTransactionId() {
+        Instant now = Instant.now();
+        return now.getEpochSecond() * 1_000_000 + now.getNano() / 1_000;
+    }
+
     public static Transaction createTransaction() {
         return Transaction.builder()
-            .transactionId(Faker.instance().number().numberBetween(1, 100))
+            .transactionId(generateTransactionId())
             .accountId(Faker.instance().number().numberBetween(1, 100))
             .amount(Faker.instance().number().numberBetween(10_000, 100_000))
             .isDebit(Faker.instance().bool().bool())
@@ -17,7 +24,7 @@ public class TransactionFaker {
 
     public static Transaction createTransaction(int accountId, int amount) {
         return Transaction.builder()
-            .transactionId(Faker.instance().number().numberBetween(1, 100))
+            .transactionId(generateTransactionId())
             .accountId(accountId)
             .amount(amount)
             .isDebit(Faker.instance().bool().bool())
@@ -26,7 +33,7 @@ public class TransactionFaker {
 
     public static Transaction createTransaction(int accountId, int amount, boolean isDebit) {
         return Transaction.builder()
-            .transactionId(Faker.instance().number().numberBetween(1, 100))
+            .transactionId(generateTransactionId())
             .accountId(accountId)
             .amount(amount)
             .isDebit(isDebit)
