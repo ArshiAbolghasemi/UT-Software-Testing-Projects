a11795ac07224d41ee4757128fae1b0f30a3f4c8
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2024-11-20 23:55:59 +0330

    feat: adding validate to search param fetch restaurants

--- Mizdooni/src/main/java/mizdooni/controllers/RestaurantController.java
+++ Mizdooni/src/main/java/mizdooni/controllers/RestaurantController.java
@@ -32,6 +32,7 @@ class RestaurantController {
     @GetMapping("/restaurants")
     public Response getRestaurants(@RequestParam int page, RestaurantSearchFilter filter) {
         try {
+            filter.validate();
             PagedList<Restaurant> restaurants = restaurantService.getRestaurants(page, filter);
             return Response.ok("restaurants listed", restaurants);
         } catch (Exception ex) {
--- Mizdooni/src/main/java/mizdooni/model/RestaurantSearchFilter.java
+++ Mizdooni/src/main/java/mizdooni/model/RestaurantSearchFilter.java
@@ -1,6 +1,7 @@
 package mizdooni.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,6 +12,24 @@ public class RestaurantSearchFilter {
     private String location;
     private String sort;
     private String order;
+
+    private static final String SORT_BY_RATING = "rating";
+    private static final String SORT_BY_REVIEWS = "reviews";
+    private static final String[] VALID_SORT_FIELDS = {SORT_BY_RATING, SORT_BY_REVIEWS};
+
+    private static final String ORDER_ASC = "asc";
+    private static final String ORDER_DESC = "desc";
+    private static final String[] VALID_ORDER = {ORDER_ASC, ORDER_DESC};
+
+    public void validate() {
+        if (order != null && !order.isEmpty() && !Arrays.asList(VALID_ORDER).contains(order)) {
+            throw new IllegalArgumentException("invalid order query param!");
+        }
+
+        if (sort != null && !sort.isEmpty() && !Arrays.asList(VALID_SORT_FIELDS).contains(sort)) {
+            throw new IllegalArgumentException("invalid sort query param!");
+        }
+    }
 
     public List<Restaurant> filter(List<Restaurant> restaurants) {
         List<Restaurant> rest = new ArrayList<>(restaurants);
@@ -24,15 +43,15 @@ public class RestaurantSearchFilter {
             rest = rest.stream().filter(r -> r.getAddress().getCity().equals(location)).collect(Collectors.toList());
         }
         if (sort != null) {
-            if (sort.equals("rating")) {
+            if (sort.equals(SORT_BY_RATING)) {
                 Comparator<Restaurant> comparator = Comparator.comparing(r -> r.getAverageRating().overall);
-                if (order != null && order.equals("asc")) {
+                if (order != null && order.equals(ORDER_ASC)) {
                     comparator = comparator.reversed();
                 }
                 rest.sort(comparator.reversed());
-            } else if (sort.equals("reviews")) {
+            } else if (sort.equals(SORT_BY_REVIEWS)) {
                 Comparator<Restaurant> comparator = Comparator.comparingInt(r -> r.getReviews().size());
-                if (order != null && order.equals("asc")) {
+                if (order != null && order.equals(ORDER_ASC)) {
                     comparator = comparator.reversed();
                 }
                 rest.sort(comparator.reversed());
