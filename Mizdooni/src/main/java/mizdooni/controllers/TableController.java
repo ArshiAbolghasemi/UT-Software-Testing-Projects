990763fd9a0aa4731597de86fef234b6420d1ff0
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2024-11-25 23:58:16 +0330

    feat: check seatsNumber is zero or not

--- Mizdooni/src/main/java/mizdooni/controllers/TableController.java
+++ Mizdooni/src/main/java/mizdooni/controllers/TableController.java
@@ -1,5 +1,6 @@
 package mizdooni.controllers;
 
+import mizdooni.exceptions.InvalidSeatsNumber;
 import mizdooni.model.Table;
 import mizdooni.response.Response;
 import mizdooni.response.ResponseException;
@@ -43,6 +44,9 @@ class TableController {
         int seatsNumber;
         try {
             seatsNumber = Integer.parseInt(params.get("seatsNumber"));
+            if (seatsNumber == 0) {
+                throw new InvalidSeatsNumber();
+            }
         } catch (Exception ex) {
             throw new ResponseException(HttpStatus.BAD_REQUEST, PARAMS_BAD_TYPE);
         }
